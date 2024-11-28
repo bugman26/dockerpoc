176bd0b6b0016c69b6b67f210c86053b65dc3bac
Eliseo M. Dayag Jr <deejay@Eliseos-MacBook-Pro.local>
2024-11-29 01:10:57 +0800

    sample

---
+++ src/sample/Main.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Hi Docker I am running!");
+
+	}
+
+}
