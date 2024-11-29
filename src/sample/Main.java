608ff55fe7032922b9aa149dcb77657cc5f7f688
Eliseo M. Dayag Jr <deejay@Eliseos-MacBook-Pro.local>
2024-11-29 15:20:41 +0800

    Lugisss

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,8 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.print("Hi Docker I am running!");
 		System.out.print("Hello I'm here in Makati");
 
 	}
